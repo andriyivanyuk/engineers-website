8c8c31de1a22fd0bfd583d541e973e2bc43d47d3
andriyivanyuk <andriyivvanyuk@gmail.com>
2024-11-17 17:57:25 +0200

    smooth added

--- js/mapStrapiContent.js
+++ js/mapStrapiContent.js
@@ -3,43 +3,58 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function fetchDataFromStrapi() {
-  const apiUrl = "http://localhost:1337/api";
+  //   const localApi = "http://localhost:1337/api";
+  const productionApiUrl =
+    "https://engineers-admin-0b4f437c3ec6.herokuapp.com/api";
 
-  const fetchHero = fetch(`${apiUrl}/hero`).then((response) => response.json());
-  const fetchServices = fetch(`${apiUrl}/services`).then((response) =>
+  const fetchHero = fetch(`${productionApiUrl}/hero`).then((response) =>
     response.json()
   );
-  const fetchNavigation = fetch(`${apiUrl}/navigations`).then((response) =>
+  const fetchServices = fetch(`${productionApiUrl}/services`).then((response) =>
     response.json()
+  );
+  const fetchNavigation = fetch(`${productionApiUrl}/navigations`).then(
+    (response) => response.json()
   );
 
   Promise.all([fetchHero, fetchServices, fetchNavigation])
     .then((data) => {
       const [heroData, servicesData, navigationData] = data;
-      this.updateHeroSection(heroData);
       this.updateNavigation(navigationData.data);
+      this.updateHeroSection(heroData);
     })
     .catch((error) => {
       console.error("Failed to fetch data:", error);
     });
 }
 
-//Method for controlling navigation
 function updateNavigation(items) {
-  const navigation = document.querySelector(".s-header__nav");
-  navigation.innerHTML = "";
-  items.forEach((item) => {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    a.textContent = item.navTitle;
-    a.href = item.link;
-    a.className = "smoothscroll";
-    a.title = item.navTitle;
-
-    li.appendChild(a);
-    navigation.appendChild(li);
-  });
-}
+    const navigation = document.querySelector(".s-header__nav");
+    navigation.innerHTML = "";
+    items.forEach((item) => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      a.textContent = item.navTitle;
+      a.href = item.link;
+      a.className = "smoothscroll";
+      a.title = item.attributeTitle;
+  
+      a.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = e.currentTarget.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: 'smooth'
+          });
+        }
+      });
+  
+      li.appendChild(a);
+      navigation.appendChild(li);
+    });
+  }
+  
 
 function updateHeroSection(items) {
   const title = document.querySelector(".hero-title");
